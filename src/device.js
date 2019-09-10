6a933c3abc7244a06e5944fb2b7fbb651eb2181c
Vladislav Korbut <v.korbut8@gmail.com>
2019-09-10 10:13:53 +0300

    remove extra listeners

---
+++ src/device.js
@@ -0,0 +1,11 @@
+export function isTouchSupported() {
+  return (
+    'ontouchstart' in window ||
+    navigator.maxTouchPoints > 0 ||
+    navigator.msMaxTouchPoints > 0
+  );
+}
+
+export function isDeviceOrientationSupported() {
+    return Boolean(DeviceOrientationEvent);
+}
