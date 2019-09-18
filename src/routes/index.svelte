2cf2d4dad647f241477e01c8b3d56655c6c5d9cd
Vladislav Korbut <v.korbut8@gmail.com>
2019-09-18 22:50:17 +0300

    update meta

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,8 +4,10 @@
 
 <svelte:head>
 	<title>Vlad Korbut | Frontend Developer</title>
-	<meta name="og:title" content="Vlad Korbut | Frontend Developer">
-	<meta name="og:image" content="https://vkrbt.me/vkrbt@2x.jpg">
+	<meta property="og:title" content="Vlad Korbut | Frontend Developer">
+	<meta property="og:image" content="https://vkrbt.me/vkrbt@2x.jpg">
 	<meta property="og:type" content="website">
+	<meta property="og:image:width" content="200" />
+	<meta property="og:image:height" content="200" />
 </svelte:head>
 <Main />
