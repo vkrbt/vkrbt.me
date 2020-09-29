456424d376130352142f275c08fe08368abb602d
Vladislav Korbut <v.korbut8@gmail.com>
2020-09-30 01:25:04 +0300

    fix cv meta

--- src/routes/me.svelte
+++ src/routes/me.svelte
@@ -3,6 +3,16 @@
     import src2x from 'images/vkrbt@2x.jpg';
 </script>
 
+<svelte:head>
+    <title>Влад Корбут | Разработчик интерфейсов</title>
+    <meta property="description" content="" />
+    <meta property="og:title" content="Влад Корбут | Разработчик интерфейсов" />
+    <meta property="og:image" content="{src2x}" />
+    <meta property="og:type" content="website" />
+    <meta property="og:image:width" content="200" />
+    <meta property="og:image:height" content="200" />
+</svelte:head>
+
 <div class="notes">
     <div>
         <img class="picture" {src} srcset="{src2x} 2x" alt="мой портрет" />
