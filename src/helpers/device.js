646370f562b07c756018329eece5c37c71a4a58f
Vladislav Korbut <vkrbt@ya.ru>
2021-12-06 13:53:14 +0300

    Update device.js

--- src/helpers/device.js
+++ src/helpers/device.js
@@ -7,3 +7,16 @@ export function isTouchSupported() {
 export function isDeviceOrientationSupported() {
     return Boolean(window.DeviceOrientationEvent);
 }
+
+export function isIOS() {
+  return [
+    'iPad Simulator',
+    'iPhone Simulator',
+    'iPod Simulator',
+    'iPad',
+    'iPhone',
+    'iPod'
+  ].includes(navigator.platform)
+  // iPad on iOS 13 detection
+  || (navigator.userAgent.includes("Mac") && "ontouchend" in document)
+}
