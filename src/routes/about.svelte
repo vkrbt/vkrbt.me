7175ed541914d59f5dc645ebfd626b77268cdeaf
Vladislav Korbut <vkorbut@yandex-team.ru>
2022-07-22 00:46:44 +0300

    update main page

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,14 +1,59 @@
-<h1>Обо мне</h1>
+<script>
+    import src2x from 'images/vkrbt@2x.jpg';
+    import {getPluralForm} from 'helpers/getPluralForm';
+    import {getMyAge} from 'helpers/getMyAge';
 
-<p>Пока я не придумал, чем наполнить эту страницу</p>
+    let ageSring = getPluralForm(getMyAge(), ['год', 'года', 'лет']);
+</script>
 
-<a href="/me/">cv</a>
-<a href="/cv/">cv detailed</a>
-<a href="/eng/">cv eng</a>
-<a href="/verbs/">irregular verbs</a>
+<section class="about">
+    <h1>Обо мне</h1>
+
+    <p>
+        Я Влад Корбут. Мне {ageSring}. Живу сейчас в Минске. Мой карьерный путь описан <a href="/me/">здесь</a>.
+    </p>
+    <p>
+        Я увлекаюсь фотографией, снимаю на пленку. Мои снимки можно <a rel='noopener noreferrer' target='_blank' href="https://www.instagram.com/vkrbt.film/">понаблюдать в инсте</a>.
+    </p>
+    <p>
+        Раньше я увлекался барной культурой, готовил коктейли дома, но сейчас перегорел. Предпочитаю белое вино и пиво, но больше всего люблю водичку.
+    </p>
+    <p>
+        Хожу на терапию, обучаюсь на гештальт-терапевта. Не планирую становиться психотерапевтом, скорее хочу получить знания и навыки для себя.
+    </p>
+    <p>Еще я админ <a href="https://t.me/vkuspi">слегка популярного канала с мемами в телеграме</a>. Приходите, вместе посмеемся.</p>
+    <p>
+        А <a rel='noopener noreferrer' target='_blank' href="https://www.instagram.com/vkrbt/">здесь моя личная инста</a>. А
+        <a href="https://t.me/vkrbt">здесь телеграм</a>.
+    </p>
+
+    <a class="hidden" href="/me/">cv</a>
+    <a class="hidden" href="/cv/">cv detailed</a>
+    <a class="hidden" href="/eng/">cv eng</a>
+    <a class="hidden" href="/verbs/">irregular verbs</a>
+</section>
+
+<svelte:head>
+    <title>Влад Корбут</title>
+    <meta property="description" content="" />
+    <meta property="og:title" content="Влад Корбут | Разработчик интерфейсов" />
+    <meta property="og:image" content="{src2x}" />
+    <meta property="og:type" content="website" />
+    <meta property="og:image:width" content="200" />
+    <meta property="og:image:height" content="200" />
+</svelte:head>
 
 <style>
-    a {
+    .about {
+        max-width: 600px;
+        margin: auto;
+    }
+
+    .hidden {
         display: none;
+    }
+
+    p + p {
+        margin-top: 1em;
     }
 </style>
