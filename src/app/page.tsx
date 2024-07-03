b73ea321227127ade8b70a92cc923341c43a74d3
Vladislav Korbut <vkrbt@ya.ru>
2024-07-03 18:18:31 +0300

    update styles and texts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,48 +16,50 @@ export default function Home() {
                     <h1>Vlad Korbut</h1>
                     <h2>Frontend developer</h2>
                     <div className={styles.contacts}>
-                            <a
-                                target='_blank'
-                                href='mailto:vkrbt@ya.ru'
-                                className={styles.email}
-                            >
-                                <span className={styles.contact}>Mail</span>
-                                <span className={styles.contactPrint}>vkrbt@ya.ru</span>
-                            </a>
-                            <a
-                                target='_blank'
-                                href='https://t.me/vkrbt'
-                            >
-                                <span className={styles.contact}>Telegram</span>
-                                <span className={styles.contactPrint}>t.me/vkrbt</span>
-                            </a>
-                            <a
-                                target='_blank'
-                                href='https://github.com/vkrbt'
-                            >
-                                <span className={styles.contact}>Github</span>
-                                <span className={styles.contactPrint}>github.com/vkrbt</span>
-                            </a>
-                            <a
-                                target='_blank'
-                                href='https://www.instagram.com/vkrbt/'
-                            >
-                                <span className={styles.contact}>Insta</span>
-                                <span className={styles.contactPrint}>instagram.com/vkrbt</span>
-                            </a>
+                        <a
+                            target='_blank'
+                            href='mailto:vkrbt@ya.ru'
+                            className={styles.email}
+                        >
+                            <span className={styles.contact}>Mail</span>
+                            <span className={styles.contactPrint}>vkrbt@ya.ru</span>
+                        </a>
+                        <a
+                            target='_blank'
+                            href='https://t.me/vkrbt'
+                        >
+                            <span className={styles.contact}>Telegram</span>
+                            <span className={styles.contactPrint}>t.me/vkrbt</span>
+                        </a>
+                        <a
+                            target='_blank'
+                            href='https://github.com/vkrbt'
+                        >
+                            <span className={styles.contact}>Github</span>
+                            <span className={styles.contactPrint}>github.com/vkrbt</span>
+                        </a>
+                        <a
+                            target='_blank'
+                            href='https://www.instagram.com/vkrbt/'
+                        >
+                            <span className={styles.contact}>Insta</span>
+                            <span className={styles.contactPrint}>instagram.com/vkrbt</span>
+                        </a>
                     </div>
                 </div>
                 <Image src='/photo.jpeg' width='160' height='160' className={styles.picture} alt='my portrait' />
             </div>
-            <h3>About</h3>
-            <p>My favourite thing in web development is to automate stuff and communicate with other people</p>
-            <p>I prefer to make things as easy to use as possible</p>
             <h3>
                 Work Experience
                 <span className={styles.experience}>{experience} years</span>
             </h3>
-            <h3  className={styles.headline}>
-                <a className={styles.headline} href='https://bookmate.ru/'>Bookmate</a>
+            <h3>About</h3>
+            <p>Passionate web developer with a focus on result achieving results, optimizing processes, improving
+                communication, and creating user-friendly experiences.</p>
+            <p>Seeking a challenging web development role with the opportunity of leadership development.</p>
+
+            <h3 className={styles.headline}>
+                <a href='https://bookmate.ru/'>Bookmate</a>
                 <span className={styles.dates}>2022 — now</span>
             </h3>
             <p className={styles.jobtitle}>Senior Frontend Developer</p>
@@ -65,13 +67,13 @@ export default function Home() {
                 <li>Made redesign and implemented design system</li>
                 <li>Improved build and release speed</li>
                 <li>Enhanced analytics tracking</li>
-                <li>Set up release flow for external team</li>
+                <li>Set up CI release flow for external team</li>
                 <li>Set up tools for easy task testing</li>
             </ul>
             <h4>Responsibilities</h4>
             <ul className={styles.experienceInfo}>
                 <li>Audio player integration</li>
-                <li>Management of technical tasks</li>
+                <li>Technical tasks management</li>
                 <li>Teammates mentoring</li>
                 <li>Everyday routine automatization</li>
                 <li>Close cooperation with a design team</li>
@@ -81,10 +83,11 @@ export default function Home() {
                 <li>Beta testing application development</li>
                 <li>UI redesign of main parts of the application</li>
             </ul>
-            <h3 className={styles.headline}><a href='https://yandex.ru/q'>Yandex.Q</a><span className={styles.dates}>2019 — 2022</span></h3>
+            <h3 className={styles.headline}><a href='https://yandex.ru/q'>Yandex.Q</a><span className={styles.dates}>2019 — 2022</span>
+            </h3>
             <p className={styles.jobtitle}>Senior Frontend Developer</p>
             <ul className={styles.experienceInfo}>
-            <li>Set up CI and deployment flow. Made deployment process simpler.</li>
+                <li>Set up CI and deployment flow. Made deployment process simpler.</li>
                 <li>Developed service for generating OpenGraph images. Improved speed and complexity of
                     development.
                 </li>
@@ -119,30 +122,31 @@ export default function Home() {
                 <li>Performance improvement</li>
                 <li>Teaching in the FDS(Frontend Development School). Students&apos; mentoring</li>
             </ul>
-            <h4>ITechArt
+            <h3 className={styles.headline}>
+                <a href="https://itechartgroup.by/">
+                    ITechArt
+                </a>
                 <span className={styles.dates}>
                 2017—2018
                 </span>
-            </h4>
+            </h3>
             <p className={styles.jobtitle}>Frontend Developer</p>
-            <ul  className={styles.experienceInfo}>
-            <li>
-                    Artworks collection application
-                    <h4>Responsibilities</h4>
-                    <ul>
-                        <li>Architecture design</li>
-                        <li>Multirole browsing development</li>
-                        <li>Development of admin panel</li>
-                    </ul>
-                </li>
-                <li>
-                    Biological Additives shop
-                    <h4>Responsibilities</h4>
-                    <ul>
-                        <li>UI redesign</li>
-                        <li>Payment form development. Stripe integration</li>
-                    </ul>
-                </li>
+            <h4>
+                Artworks collection application
+            </h4>
+            <h4>Responsibilities</h4>
+            <ul className={styles.experienceInfo}>
+                <li>Architecture design</li>
+                <li>Multirole browsing development</li>
+                <li>Development of admin panel</li>
+            </ul>
+            <h4>
+                Biological Additives shop
+            </h4>
+            <h4>Responsibilities</h4>
+            <ul className={styles.experienceInfo}>
+                <li>UI redesign</li>
+                <li>Payment form development. Stripe integration</li>
             </ul>
             <h3 className={styles.headline}>Education</h3>
             <ul className={styles.experienceInfo}>
@@ -162,6 +166,7 @@ export default function Home() {
                 <span>Apollo</span>
                 <span>Mobx</span>
                 <span>Redux</span>
+                <span>CI</span>
                 <span>Webstorm</span>
                 <span>ZSH</span>
                 <span>Prettier</span>
